96c28f60b79988477e24492bdafa23c4ae7b0114
Berk Dogan <berk@tekbyte.net>
2023-06-26 14:34:58 +1000

    Cleanup

--- Infinity/Infinity/InfinityApp.swift
+++ Infinity/Infinity/InfinityApp.swift
@@ -6,23 +6,6 @@
 
 import SwiftUI
 
-
-/**
- Identify the device our app is running on
- */
-extension UIDevice {
-    static var idiom: UIUserInterfaceIdiom {
-        UIDevice.current.userInterfaceIdiom
-    }
-    static var isiPad: Bool {
-        idiom == .pad
-    }
-    
-    static var isiPhone: Bool {
-        idiom == .phone
-    }
-}
-
 @main
 struct InfinityApp: App {
     @StateObject var storeVM = StoreViewModel()
