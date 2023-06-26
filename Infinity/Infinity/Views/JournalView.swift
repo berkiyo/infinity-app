d16d4ec704e2b0784f95bfda5fc5c9d9dada875f
Berk Dogan <berk@tekbyte.net>
2023-06-26 10:19:20 +1000

    Code cleanup for RevenueCat

---
+++ Infinity/Infinity/Views/JournalView.swift
@@ -0,0 +1,20 @@
+//
+//  JournalView.swift
+//  Infinity
+//
+//  Created by Berk Dogan on 9/6/2023.
+//
+
+import SwiftUI
+
+struct JournalView: View {
+    var body: some View {
+        Text("Coming soon!")
+    }
+}
+
+struct JournalView_Previews: PreviewProvider {
+    static var previews: some View {
+        JournalView()
+    }
+}
--- Infinity/Infinity/Views/SettingsView.swift
+++ Infinity/Infinity/Views/SettingsView.swift
@@ -3,8 +3,6 @@ import StoreKit
 
 struct SettingsView: View {
     @Environment(\.requestReview) var requestReview
-    @EnvironmentObject var storeVM: StoreViewModel
-    
     
     var body: some View {
         ZStack {
@@ -12,81 +10,57 @@ struct SettingsView: View {
                 NavigationStack {
                     List {
                         // Guide View
-                        NavigationLink { GuideView() } label: {
-                            Text("❓ How To Guide")
-                        }
-                        
-                        
-                        // Pro View
-                        // Removed, will probably add it in a later version.
-                        /*
-                         NavigationLink { ProView() } label: {
-                         Text("💎 Infinity Pro")
-                         }
-                        
-                        */
-                        
-                        // Icon Changer View
-                        // Pro Feature
-                        NavigationLink { ChangeAppIconView() } label: {
-                            Text("📱 Change App Icon")
-                        }
-                        
-                        
-                        // Apple Watch Complications View
-                        // For future release
-                        /*
-                        NavigationLink { AppleWatchView() } label: {
-                            Text("⌚️ Apple Watch (Coming soon)")
-                        }
-                        .disabled(true)
-                        */
-                        
-                        
-                        // Quotes View
-                        // Pro Feature
-                        // This will come in the next version, for now, we'll focus on cleanup.
-                        /*
-                        NavigationLink { QuotesView() } label: {
-                            Text("😤 Quotes (Coming soon)")
-                        }
-                        .disabled(true)
-                        */
-                        // Notification View
-                        // Pro Feature
-                        NavigationLink { NotificationView() } label: {
-                            Text("🔔 Custom Notifications")
-                        }
-                         
-                        // About View
-                        NavigationLink { AboutView() } label: {
-                            Text("🧐 About Infinity")
-                        }
-                        
-                        
-                        // Feedback Hyperlink (Email)
-                        Link("✉️ Give Feedback", destination: URL(string: "mailto:mail@berkdogan.com")!)
-                        
-                        
-                        // Review prompt
-                        Button("⭐️ Leave a review") {
-                            requestReview()
-                        }
-                        
-                        // DEV PURPOSES
-                        // For API Scraping
-                        /*
-                        Button("Ping Quote") {
-                            Task {
-                                do {
-                                    try await getQuotes()
-                                } catch {
-                                    print("Error, please try again.", error)
-                                }
+                        Section(header: Text("General")) {
+                            // Guide view
+                            NavigationLink { GuideView() } label: {
+                                Text("❓ How To Guide")
+                            }
+                            // About View
+                            NavigationLink { AboutView() } label: {
+                                Text("🧐 About Infinity")
                             }
                         }
-                        */
-                        // Just running some tests
+                        
+                        
+                        Section(header: Text("Pro Settings")) {
+                            // Removed, will probably add it in a later version.
+                             NavigationLink { ProView() } label: {
+                                 Text("💎 Infinity Pro")
+                             }
+                            
+                            // Icon Changer View
+                            // Pro Feature
+                            NavigationLink { ChangeAppIconView() } label: {
+                                Text("📱 Change App Icon")
+                            }
+                            
+                            // Notification View
+                            // Pro Feature
+                            NavigationLink { NotificationView() } label: {
+                                Text("🔔 Custom Notifications")
+                            }
+                            
+                            // Quotes View
+                            // Pro Feature
+                            NavigationLink { QuotesView() } label: {
+                                Text("📖 Motivational Quotes")
+                            }
+                            
+                            NavigationLink { JournalView() } label: {
+                                Text("📝 Personal Journal")
+                            }
+                        }
+                        
+                        Section(header: Text("Feedback")) {
+                            // Feedback Hyperlink (Email)
+                            Link("✉️ Give Feedback", destination: URL(string: "mailto:mail@berkdogan.com")!)
+                            
+                            
+                            // Review prompt
+                            Button("⭐️ Leave a review") {
+                                requestReview()
+                            }
+                        }
                         
                         
                     } // END LIST
@@ -94,33 +68,5 @@ struct SettingsView: View {
             } // END VSTACK
         } // END ZSTACK
     }
-    
-    /**
-     getQuotes()
-     This is used for getting the quotes from the internet.
-     For the API, we are using "https://api.goprogram.ai/inspiration/" as it is open.
-     We want this behaviour:
-     
-     - fetch quotes periodically
-     - send out notifications periodically (user decides)
-     - This can work with the widget
-     */
-    
-    func getQuotes() async throws {
-        
-        struct Quote: Decodable {
-            var quote: String
-            var author: String
-        }
-        
-        guard let url = URL(string: "https://api.goprogram.ai/inspiration/") else { fatalError("Unable to fetch quote, please try again later.") }
-            let urlRequest = URLRequest(url: url)
-            let (data, response) = try await URLSession.shared.data(for: urlRequest)
 
-            guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data")
-            }
-        let decodedFood = try JSONDecoder().decode(Quote.self, from: data)
-        print(decodedFood)
-    }
-    
 }
